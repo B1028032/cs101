fa6b98ce0e45795b21d79ad1d8da161fd4a85c90
KO-HAN <151459079+B1028032@users.noreply.github.com>
2023-12-20 21:58:41 +0800

    Update main1.c

--- hw5/main1.c
+++ hw5/main1.c
@@ -1,29 +1,19 @@
 #include <stdio.h>
 
-
 int main()
 {
     int n = 7;
-    int rowCount = 1;
 
-
-
-    for (n; n > 0; n--)
+    for (int i = 1; i <= n; i++)
     {
-
         for (int j = 1; j <= n; j++)
         {
-            printf(" ");
+            if (j <= n - i)
+                printf(" ");
+            else
+                printf("%d ", i);
         }
-
-        for (int j = 1; j <= rowCount; j++)
-        {
-            printf("%d ", rowCount);
-        }
-
         printf("\n");
-
-        rowCount++;
     }
 
     return 0;
